6a2f095e2f122ed656626561e6e8aa0613d71f86
Lucas Santos <lucazwebmail@gmail.com>
2025-08-26 21:30:12 -0300

    feat: setup typeorm and docker-compose
    - setup docker compose
    - setup typerom initial config

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
 import express from "express";
 import cors from "cors";
 import { routes } from "./routes";
-
+import { AppDataSource } from "./database";
 const PORT = 8080;
-const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
+AppDataSource.initialize()
+  .then(() => {
+    const app = express();
+
+    app.use(
+      cors({
+        origin: "http://localhost:5173",
+      })
+    );
+    app.use(express.json());
+    app.use(routes);
+
+    app.listen(PORT, () => {
+      console.log(`running on port ${PORT}`);
+    });
   })
-);
-app.use(express.json());
-app.use(routes);
-
-app.listen(PORT, () => {
-  console.log(`running on port ${PORT}`);
-});
+  .catch((error) => console.log(error));
