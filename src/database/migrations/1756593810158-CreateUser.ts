67e40bb29ecbb47f292ee0cf03ae24f08ffb264e
lucazweb <lucazwebmail@gmail.com>
2025-08-30 20:26:54 -0300

    chore: add create user migration

---
+++ src/database/migrations/1756593810158-CreateUser.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUser1756593810158 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "email",
+            type: "varchar",
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
