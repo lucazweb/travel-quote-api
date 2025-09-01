5630c920b418e40e5a32e75f4cfe1cda98416215
lucazweb <lucazwebmail@gmail.com>
2025-08-31 22:23:57 -0300

    refactor: AppError class

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -1,8 +1,8 @@
-class AppError {
-  private message: string;
-  private statusCode: number;
+class AppError extends Error {
+  private readonly statusCode: number;
 
   constructor(message: string, statusCode: number = 500) {
+    super(message);
     this.message = message;
     this.statusCode = statusCode;
   }
