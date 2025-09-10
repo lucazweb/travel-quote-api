3b852e30c2c560e3a3d75bde7357e4cb43a708ab
lucazweb <lucazwebmail@gmail.com>
2025-09-09 23:09:58 -0300

    feat: swagger docs

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { routes } from "../routes";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocs from "../../../../swagger.json";
 
 const app = express();
 
@@ -11,5 +13,6 @@ app.use(
 );
 app.use(express.json());
 app.use(routes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 export { app };
